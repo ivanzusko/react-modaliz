112945bb696f84c882bf199bfb52cbc68e9918e5
Ivan Zusko <IvanZusko@ecenglish.com>
2017-02-22 12:50:34 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# React Modaliz
+React component which provide nice and simple modal pop-up window
+
+## Installation
+Just run
+```javascript
+npm i react-modaliz
+```
+or (if you are using __Yarn__)
+```javascript
+yarn add react-modaliz
+```
+
+## Usage
+```javascript
+import Modaliz from 'react-modaliz';
+
+<Modaliz
+    className="any-custom-class"
+    onClose={this.closeModal}
+    show={this.state.showModal}
+    speed={500}
+>
+    <h1>You title</h1>
+    <div>
+        <p>Lorem inpsum</p>
+    </div>
+</Modaliz>
+```
+
+## Customization
+
+Property | Type | isRequired | Default value | Description
+:---|:---|:---|:---|:---
+`className` | string | `false` | | Set your className which will get suffix `-wrapper` for whole modal popup component, suffix `-container` for modal-box and suffix `-overlay` for overlay. E.g.: `yourName-wrapper`, `yourName-container`, `yourName-close` and `yourName-overlay`
+`conditionalRender` | boolean | `false` | `false` | in some cases, if you need conditional render (e.g.: `{ifSomethingTrue && <Modaliz></Modaliz>}`) you should add `conditionalRender` property to `Modaliz` component
+`discardDefaults` | boolean | `false` | `false` | Set it to be `true` if you very badly want to discard all default styles
+`onClose` | Function | `true` |  | Handler for closing the modal window
+`speed` | number | `false` | `100` | Speed of the fading in effect
+
+
+### P.S.
+Huge appreciation to [JULIAN ĆWIRKO](http://julian.io/) for his [react-npm-boilerplate](https://github.com/juliancwirko/react-npm-boilerplate)
---
+++ src/index.js
@@ -0,0 +1,150 @@
+/**
+ * Modaliz
+ */
+
+import React, { Component, PropTypes } from 'react';
+
+const propTypes = {
+    className:          PropTypes.string,
+    conditionalRender:  PropTypes.bool,
+    discardDefaults:    PropTypes.bool,
+    speed:              PropTypes.number,
+    onClose:            PropTypes.func.isRequired,
+}
+
+const defaultProps = {
+    conditionalRender:  false,
+    discardDefaults:    false,
+    speed:              100,
+}
+
+class Modaliz extends Component {
+    constructor () {
+        super();
+
+        this.state = {
+            fadeIn: {}
+        }
+    }
+
+    componentDidMount () {
+        if (this.props.conditionalRender) {
+            setTimeout(() => {
+                this.setState({
+                    fadeIn: {
+                        opacity:    1,
+                        transition: `${this.props.speed}ms`,
+                    }
+                });
+            }, 1);
+        }
+    }
+
+    componentWillReceiveProps (nextProps) {
+        if (!this.props.conditionalRender) {
+            if (nextProps.show) {
+                setTimeout(() => {
+                    this.setState({
+                        fadeIn: {
+                            opacity:    1,
+                            transition: `${this.props.speed}ms`,
+                        }
+                    });
+                }, 1);
+            }
+            else {
+                this.setState({ fadeIn: {} });
+            }
+        }
+    }
+
+    render () {
+        const wrapper = this.props.className ? `modaliz-wrapper ${this.props.className}-wrapper` : 'modaliz-wrapper';
+        const container = this.props.className ? `modaliz-container ${this.props.className}-container` : 'modaliz-container';
+        const close = this.props.className ? `modaliz-close ${this.props.className}-close` : 'modaliz-close';
+        const overlay = this.props.className ? `modaliz-overlay ${this.props.className}-overlay` : 'modaliz-overlay';
+        let modalizStyle = {};
+
+        if (!this.props.discardDefaults) {
+            modalizStyle = {
+                wrapper: {
+                    position:   'fixed',
+                    width:      '100vw',
+                    height:     '100vh',
+                    zIndex:     1000,
+                    top:        0,
+                    left:       0,
+                    opacity:    0,
+                },
+                container: {
+                    position:           'absolute',
+                    maxWidth:           '500px',
+                    minHeight:          '100px',
+                    width:              '90vw',
+                    backgroundColor:    '#ffffff',
+                    top:                '50%',
+                    left:               '50%',
+                    transform:          'translate(-50%, -50%)',
+                    zIndex:             1,
+                },
+                close: {
+                    position:       'absolute',
+                    width:          '1.5rem',
+                    height:         '1.5rem',
+                    top:            '.5rem',
+                    right:          '.5rem',
+                    fontSize:       '30px',
+                    display:        'flex',
+                    alignItems:     'center',
+                    justifyContent: 'center',
+                    cursor:         'pointer'
+                },
+                overlay: {
+                    position:           'absolute',
+                    width:              '100%',
+                    height:             '100%',
+                    backgroundColor:    'rgba(0, 0, 0, .8)',
+                    top:                0,
+                    left:               0,
+                    zIndex:             0,
+                },
+            };
+        }
+
+        if (!this.props.show) {
+            return false;
+        }
+
+        return (
+            <div
+                className={wrapper}
+                style={Object.assign({}, modalizStyle.wrapper, this.state.fadeIn)}
+            >
+                <div
+                    className={container}
+                    style={modalizStyle.container}
+                >
+                    <div
+                        className={close}
+                        style={modalizStyle.close}
+                        onClick={this.props.onClose}
+                    >
+                        &times;
+                    </div>
+                    {this.props.children}
+                </div>
+                <div
+                    className={overlay}
+                    style={modalizStyle.overlay}
+                    onClick={this.props.onClose}
+                >
+                </div>
+            </div>
+        );
+    }
+}
+
+Modaliz.propTypes = propTypes;
+Modaliz.defaultProps = defaultProps;
+
+export default Modaliz;
